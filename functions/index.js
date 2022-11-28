af5b087d6e32f49a5345a9f793b0c14430f771b8
Aditya Kumar Kasaudhan <74228301+Aditya8840@users.noreply.github.com>
2022-11-28 20:11:28 +0530

    firestore

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-
+const qs = require('querystring');
 const admin = require("firebase-admin");
 admin.initializeApp();
 
@@ -14,12 +14,28 @@ app.get('/', (res, req)=>{
 
 app.post('/', async (req, res)=>{
     const users = req.body;
-    const userObj = JSON.parse(users);
-    const email = userObj["email"];
+    // const userObj = JSON.stringify(users);
+    const email = users["email"];
+    delete users["email"];
+    const year = users["year"];
+    delete users["year"];
+    const day = users["day"];
+    delete users["day"];
+    const month = users["month"];
+    delete users["month"];
+    const regNo = users["regNo"];
+    delete users["regNo"];
+    const idTimeSet = users["idTimeSet"];
+    delete users["idTimeSet"];
+    const time = new Date();
+    var tm = time.getMilliseconds()
 
-    await admin.firestore().collection('test').doc(email).set(user);
+    var obj = {
+        'code': 201
+    }
+    await admin.firestore().collection('users').doc(email).collection('vDetails').doc(regNo).collection(year).doc(month).collection(day).doc(idTimeSet).set(users,{merge: true});
 
-    res.status(201).send();
+    res.status(201).send(obj);
 });
 
 exports.users = functions.https.onRequest(app);
